3ac35ae9c9f0c11ba0681dd6360747ecf2a65bd9
Mohammed Eltaweel <31571400+mohamedmagdy94@users.noreply.github.com>
2022-01-26 17:36:07 +0200

    Coordinator

--- Challenge/Module/Ingredients/Presentation/Coordinator/AddIngredientsCoordinator.swift
+++ Challenge/Module/Ingredients/Presentation/Coordinator/AddIngredientsCoordinator.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import UIKit
+import Rswift
 
-protocol AddIngredientsCoordinating{
+
+protocol AddIngredientsCoordinating: Coordinating{
     func analyzeText(with service: ExtractingIngredients)
+}
+
+class AddIngredientsCoordinator: AddIngredientsCoordinating{
+    var navigationContoller: UINavigationController
+    
+    init(navigationContoller: UINavigationController){
+        self.navigationContoller = navigationContoller
+    }
+    
+    func start() {
+        let storyboard = R.storyboard.ingredients
+        guard let view = storyboard.addIngredientsViewController() else{ return }
+        navigationContoller.pushViewController(view, animated: true)
+    }
+    
+    func analyzeText(with service: ExtractingIngredients) {
+        
+    }
 }
